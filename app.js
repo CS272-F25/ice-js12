719c009fee97b1a4e9a77c043033dbce4fcbbc55
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-11-10 15:21:19 -0600

    clearer instructions

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const NUMS = [2, 5, 6, 8, 19, 22, 27];
  *  Add event listeners such that the buttons with the following ids
  *  perform the corresponding calculation on the lists of NAMES and NUMS.
  *
- * `btn-log-names`       Prints out each name on a new line
- * `btn-first-initial`   Prints out the list of each person's first initial
- * `btn-length-of-name`  Prints out the list of each person's name length
- * `btn-j-names`         Prints out the list of people who's name starts with J
- * `btn-long-j-names`    Prints out the list of people who's name starts with J and have a long name (>= 5 characters)
- * `btn-is-there-7`      Prints out whether or not there is a 7 in the list of numbers
- * `btn-is-all-positive` Prints out whether or not each number is positive
+ * `btn-log-names`       console.log out each name (on a new line)
+ * `btn-first-initial`   Call writeOutput with the list of each person's first initial
+ * `btn-length-of-name`  Call writeOutput with the list of each person's name length
+ * `btn-j-names`         Call writeOutput with the list of people who's name starts with J
+ * `btn-long-j-names`    Call writeOutput with the list of people who's name starts with J and have a long name (>= 5 characters)
+ * `btn-is-there-7`      Call writeOutput with whether or not there is a 7 in the list of numbers
+ * `btn-is-all-positive` Call writeOutput with whether or not each number is positive
  */
 
 function writeOutput(stringOrArray) {
